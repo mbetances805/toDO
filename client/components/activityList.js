16c3669657efb40c083ae3ecb2bd72f0a547750f
Maria Betances <mbetances1002@gmail.com>
2018-02-13 22:21:02 -0500

    modification to css and activity list

--- client/components/activityList.js
+++ client/components/activityList.js
@@ -69,6 +69,8 @@ class ActivityList extends Component {
       } else {
         link = '';
         endOfText = '';
+        textSub = '';
+        updatedText = '';
         return text
       }
     };
